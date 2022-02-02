0e4220dc25cc90573884ecb5c84abf6048149812
makuke1234 <makuke1234@gmail.com>
2022-02-03 00:20:58 +0200

    Minor optimisatiolns

--- src/w_profiler/attoProfiler.c
+++ src/w_profiler/attoProfiler.c
@@ -56,17 +56,12 @@ static int curStackLen = 0;
 
 void profilerStart(void)
 {
-	if (curStackLen < PROFILER_STACK_SIZE)
-	{
-		profilerStack[curStackLen++] = clock();
-	}
+	assert(curStackLen < PROFILER_STACK_SIZE);
+	profilerStack[curStackLen++] = clock();
 }
 void profilerEnd(const char * funcName)
 {
-	if (curStackLen == 0)
-	{
-		return;
-	}
+	assert(curStackLen > 0);
 	--curStackLen;
 	writeProfiler(funcName, "Elapsed %.3f s", (double)(clock() - profilerStack[curStackLen]) / (double)CLOCKS_PER_SEC);
 }
